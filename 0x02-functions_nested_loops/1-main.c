70c77921f03aae14a7a51c805b528b2a1b055bc1
mogotladee <kelesebake@gmail.com>
2023-07-18 13:48:27 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main - check the code
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
     print_alphabet();
-    return 0;
+    return (0);
 }
-
