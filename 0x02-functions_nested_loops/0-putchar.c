bb613b2575fb8f8ba65c8c60088ed07831fa6432
mogotladee <kelesebake@gmail.com>
2023-07-18 04:59:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return 0;
+}
+
+/* Your implementation of _putchar() */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabert(void);
+void print_alphabert_x10(void)
+int _islover(int c);
+int _isalpha(int c);
+int _sign(int n);
+int _aas(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int,int);
+void print_to_98(int n);
+void print_time_table(int n);
