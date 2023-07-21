efc32925e343c45de161f11866c4eccdc660d2a0
mogotladee <kelesebake@gmail.com>
2023-07-21 07:47:47 -0700

    100-prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,47 +1,25 @@
 #include <stdio.h>
 #include <math.h>
 
-// Function to check if a number is prime
-int is_prime(long long num) {
-    if (num < 2)
-        return 0;
+/**
+ * main - main block
+ * Description: Find and print the largest prime factor of the number
+ * 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+	long num = 612852475143;
 
-    for (long long i = 2; i <= sqrt(num); i++) {
-        if (num % i == 0)
-            return 0;
-    }
+	for (c = (int) sqrt(num); c > 2; c++)
+	{
+		if (num % c == 0)
+		{
+			printf("%d\n", c);
+			break;
+		}
+	}
 
-    return 1;
+	return (0);
 }
-
-// Function to find the largest prime factor
-long long largest_prime_factor(long long num) {
-    long long largest_factor = -1;
-
-    while (num % 2 == 0) {
-        largest_factor = 2;
-        num /= 2;
-    }
-
-    for (long long i = 3; i <= sqrt(num); i += 2) {
-        while (num % i == 0) {
-            largest_factor = i;
-            num /= i;
-        }
-    }
-
-    if (num > 2)
-        largest_factor = num;
-
-    return largest_factor;
-}
-
-int main() {
-    long long num = 612852475143LL;
-    long long largest_factor = largest_prime_factor(num);
-
-    printf("%lld\n", largest_factor);
-
-    return 0;
-}
-
