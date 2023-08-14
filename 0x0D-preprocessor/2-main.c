d997e3ac462890ceb4acf5703c2fcf2658a4f9fa
mogotladee <kelesebake@gmail.com>
2023-08-14 01:16:00 -0700

    just coding Alx

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define NAME_H
+
+/**
+ * main - print the name of the file
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	#ifdef NAME_H
+		printf("%s\n", __FILE__);
+	#endif
+	return (0);
+}
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,7 @@
+#ifndef main_H
+#define main_H
+#include <stdio.h>
+
+int _putchar(char c);/*prototype _putchar*/
+
+#endif
